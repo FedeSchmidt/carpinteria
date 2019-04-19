22d5d179ecb4cf623bbbca1957674b48d252a180
FedeSchmidt <fschmidt.10@hotmail.com>
2019-04-19 12:06:39 -0300

    cambios en guitarras y venta

--- src/app/guitarras/guitarras.component.ts
+++ src/app/guitarras/guitarras.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import Datos from '../../assets/instrumentos.json';
+import { HostListener } from '@angular/core';
+import { BrowserStack } from 'protractor/built/driverProviders';
 
 @Component({
 	selector: 'app-guitarras',
@@ -21,5 +23,88 @@ export class GuitarrasComponent implements OnInit {
 
 		var datos = JSON.stringify(Datos);
 		this.data = JSON.parse(datos);
+	}
+
+	ngAfterViewInit() {
+		$('.image2').click(function(e) {
+			var width = $(window).width();
+
+			if (width > 991) {
+				$('#modalVenta').css('display', 'block');
+
+				let src = e.target.attributes.getNamedItem('src').textContent;
+				$('#img01').attr('src', src);
+			}
+		});
+
+		$('.close').click(function() {
+			$('#modalVenta').css('display', 'none');
+		});
+
+		$('#modalVenta').on('click', function(e) {
+			if (!$('#btn_izquierda').is(e.target) && !$('#btn_derecha').is(e.target) && !$('#img01').is(e.target)) {
+				$('#modalVenta').css('display', 'none');
+			}
+		});
+	}
+
+	hallarSiguiente() {
+		var src = $('#img01').attr('src');
+
+		var siguiente;
+		var i;
+		var nueva = src;
+		let items = Object.values(this.data);
+		var array = [];
+		for (let i = 0; i < items.length; i++) {
+			array.push.apply(array, items[i]);
+		}
+
+		for (let i = 0; i < array.length; i++) {
+			if (array[i] === src) {
+				siguiente = (i + 1) % array.length;
+				nueva = array[siguiente];
+				break;
+			}
+		}
+
+		$('#img01').attr('src', nueva);
+	}
+
+	hallarAnterior() {
+		var src = $('#img01').attr('src');
+
+		var anterior;
+		var i;
+		var nueva = src;
+
+		let items = Object.values(this.data);
+		var array = [];
+		for (let i = 0; i < items.length; i++) {
+			array.push.apply(array, items[i]);
+		}
+
+		for (let i = 0; i < array.length; i++) {
+			if (array[i] === src) {
+				anterior = (i - 1 + array.length) % array.length;
+				nueva = array[anterior];
+				break;
+			}
+		}
+
+		$('#img01').attr('src', nueva);
+	}
+
+	@HostListener('document:keyup', [ '$event' ])
+	handleDeleteKeyboardEvent(event: KeyboardEvent) {
+		if ($('#myModal').css('display') !== 'none') {
+			if (event.key === 'ArrowRight') this.hallarSiguiente();
+			else {
+				if (event.key === 'ArrowLeft') this.hallarAnterior();
+				else {
+					if (event.key === 'Escape') $('#myModal').css('display', 'none');
+				}
+			}
+		}
 	}
 }
