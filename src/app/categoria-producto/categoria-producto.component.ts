730713aa7570a54befb4cce9fbd85bba12bc74f7
FedeSchmidt <fschmidt.10@hotmail.com>
2019-04-24 12:58:12 -0300

    Cambios fonts

--- src/app/categoria-producto/categoria-producto.component.ts
+++ src/app/categoria-producto/categoria-producto.component.ts
@@ -1,136 +1,95 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
-import { Title }     from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';
 import Datos from '../../assets/datos.json';
 
-
-
 @Component({
-  providers:[HeaderComponent ],
-  selector: 'app-categoria-producto',
-  templateUrl: './categoria-producto.component.html',
-  styleUrls: ['./categoria-producto.component.css']
+	providers: [ HeaderComponent ],
+	selector: 'app-categoria-producto',
+	templateUrl: './categoria-producto.component.html',
+	styleUrls: [ './categoria-producto.component.css' ]
 })
-
-
 export class CategoriaProductoComponent implements OnInit {
+	public productos: Array<Array<String>>;
+	public data = JSON.stringify(Datos);
+	public datos = JSON.parse(this.data);
+	filtroActivo;
 
-   public productos: Array<Array<String>>;
-   public data = JSON.stringify(Datos);
-   public datos = JSON.parse(this.data);
-   filtroActivo;
-  
-  constructor(private comp: HeaderComponent, private titleService: Title) { 
-  
-    this.productos= [];
+	constructor(private comp: HeaderComponent, private titleService: Title) {
+		this.productos = [];
 
-    this.cargarProductos(this.datos);
+		this.cargarProductos(this.datos);
+	}
 
+	ngOnInit() {
+		$('#galeria').css('textDecoration', 'underline');
 
-    
-  }
+		this.titleService.setTitle('Carpinteria Schmidt | Galeria');
+	}
 
-  ngOnInit() {
-  
-     $("#galeria").css("textDecoration", "underline");
+	cargarProductos(datos) {
+		var c, objeto, producto;
 
-     this.titleService.setTitle( "Carpinteria Schmidt | Galeria" );
+		for (let cat in Object.keys(datos)) {
+			c = Object.keys(datos)[cat];
 
+			if (c !== 'tirantes' && c !== 'machimbre' && c !== 'vigas' && c !== 'venta-otros') {
+				for (let i in datos[c]) {
+					objeto = datos[c][i];
+					//Arreglo tiene [id, titulo, descripcion, imagen 1 y categoria]
+					producto = [ objeto.id, objeto.titulo, objeto.descripcion, objeto.imagenes[0], c ];
 
-  }
+					this.productos.push(producto);
+				}
+			}
+		}
+	}
 
-  cargarProductos(datos){
+	mostrarTodos() {
+		if (this.filtroActivo !== null) {
+			$('#' + this.filtroActivo).toggleClass('activo');
+		}
+		$('#btnTodos').toggleClass('activo');
+		this.filtroActivo = 'btnTodos';
 
-    var c, objeto, producto;
+		this.productos = [];
 
-    for(let cat in Object.keys(datos)){
-      
+		this.cargarProductos(this.datos);
+	}
 
-        c = Object.keys(datos)[cat];
+	filtrar(categoria, id) {
+		if (this.filtroActivo !== null) {
+			$('#' + this.filtroActivo).toggleClass('activo');
+		}
+		$('#' + id).toggleClass('activo');
+		this.filtroActivo = id;
 
-        if(c !== "tirantes" && c !== "machimbre"){
+		var columna, imagen, texto, objeto, producto;
 
-          for(let i in datos[c]){
-            objeto = datos[c][i];
-            //Arreglo tiene [id, titulo, descripcion, imagen 1 y categoria]
-            producto = [objeto.id, objeto.titulo, objeto.descripcion, objeto.imagenes[0], c];
+		this.productos = [];
 
-            this.productos.push(producto);
+		var arreglo = this.datos[categoria];
 
-          }
-      }
-      
-    }
+		if (arreglo !== undefined) {
+			if (arreglo.length > 0) {
+				for (let i in arreglo) {
+					objeto = arreglo[i];
 
-  }
+					producto = [ objeto.id, objeto.titulo, objeto.descripcion, objeto.imagenes[0], categoria ];
 
-  
+					this.productos.push(producto);
+				}
+			}
+		}
+	}
 
-  mostrarTodos(){
+	click() {
+		this.comp.click('contacto');
+	}
 
-    
-  if(this.filtroActivo !== null){
-      $("#"+this.filtroActivo).toggleClass("activo");
-    }
-    $("#btnTodos").toggleClass("activo");
-    this.filtroActivo = "btnTodos";
-
-
-    this.productos = [];
-
-    this.cargarProductos(this.datos);
-  }
-
-
-  filtrar(categoria, id){
-
-
-    if(this.filtroActivo !== null){
-      $("#"+this.filtroActivo).toggleClass("activo");
-    }
-    $("#"+id).toggleClass("activo");
-    this.filtroActivo = id;
-
-
-
-    var columna, imagen, texto, objeto, producto;
-
-    this.productos = [];
-
-    var arreglo = this.datos[categoria];
-
-    if(arreglo !== undefined){
-
-        if(arreglo.length > 0){
-
-          for(let i in arreglo){
-
-            objeto = arreglo[i];
-
-            producto = [objeto.id, objeto.titulo, objeto.descripcion, objeto.imagenes[0], categoria];
-
-            this.productos.push(producto);
-
-          }
-        }
-    }
-    
-    
-  }
-
-  click(){
-    this.comp.click('contacto');
-  }
-
-  clickDropdown(){
-    
-
-      $("#flechaUp").toggleClass("no-flecha");
-      $("#flechaDown").toggleClass("no-flecha");
-    
-
-  }
-
-
+	clickDropdown() {
+		$('#flechaUp').toggleClass('no-flecha');
+		$('#flechaDown').toggleClass('no-flecha');
+	}
 }
